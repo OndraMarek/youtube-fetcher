6789edc6ccd9389166b171e7ea8be728e51067e5
OndraMarek <czondrejmarek@gmail.com>
2026-06-05 21:41:42 +0200

    Improve YouTube channel handle input and response handling

--- youtube-fetcher/Program.cs
+++ youtube-fetcher/Program.cs
@@ -8,8 +8,8 @@ IConfigurationRoot config = new ConfigurationBuilder()
 
 string? youtubeApiKey = config["YoutubeApiKey"] ?? throw new Exception("Youtube API key not found!");
 
-Console.Write("Enter youtube channel handle: ");
-string channelHandel = Console.ReadLine();
+Console.Write("Enter youtube channel handle (@Handle): @");
+string channelHandle = Console.ReadLine();
 
 using HttpClient client = new();
 
@@ -18,14 +18,20 @@ client.DefaultRequestHeaders.Accept.Add(
     new MediaTypeWithQualityHeaderValue("application/json"));
 client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-await GetChannelID(client, youtubeApiKey, channelHandel);
+await GetChannelID(client, youtubeApiKey, channelHandle);
 
-static async Task GetChannelID(HttpClient client, string? youtubeApiKey, string channelHandel)
+static async Task GetChannelID(HttpClient client, string? youtubeApiKey, string channelHandle)
 {
-    var response = await client.GetFromJsonAsync<YouTubeResponse>($"https://www.googleapis.com/youtube/v3/channels?part=id&forHandle={channelHandel}&key={youtubeApiKey}");
-
-    foreach (var item in response?.Items ?? Enumerable.Empty<YouTubeItem>())
-        Console.WriteLine(item.Id);
+    var response = await client.GetFromJsonAsync<YouTubeResponse>($"https://www.googleapis.com/youtube/v3/channels?part=id&forHandle={channelHandle}&key={youtubeApiKey}");
+    var channel = response?.Items?.FirstOrDefault();
+    if (channel != null)
+    {
+        Console.WriteLine($"Saved channel ID: {channel.Id}     Link to youtube channel: https://www.youtube.com/channel/{channel.Id}");
+    }
+    else
+    {
+        Console.WriteLine("Channel not found.");
+    }
 }
 
 public record class YouTubeResponse(List<YouTubeItem> Items);
